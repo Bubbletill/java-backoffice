88b2eaa8d103947e4592e307424af911b7a0073f
IsGabriellaCurious <gabriellakaitlyn@outlook.com>
2022-06-04 23:28:12 +0100

    Lets make the application exitable!

--- src/main/java/store/bubbletill/backoffice/controllers/BOHomeController.java
+++ src/main/java/store/bubbletill/backoffice/controllers/BOHomeController.java
@@ -2,6 +2,7 @@ package store.bubbletill.backoffice.controllers;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import store.bubbletill.backoffice.BOApplication;
@@ -15,6 +16,7 @@ public class BOHomeController {
 
     @FXML private Pane homePane;
     @FXML private Label homeNameLabel;
+    @FXML private Button homeExitButton;
 
     // Top status bar
     @FXML private Label dateTimeLabel;
@@ -54,6 +56,8 @@ public class BOHomeController {
         operatorLabel.setText(app.operator.getOperatorId());
 
         homeNameLabel.setText("Welcome, " + app.operator.getName() + ".");
+
+        homeExitButton.requestFocus();
     }
 
     private void showError(String error) {
@@ -65,6 +69,11 @@ public class BOHomeController {
         errorPane.setVisible(true);
         errorLabel.setText(error);
         BOApplication.buzzer("double");
+    }
+
+    @FXML
+    private void onHomeExitButtonPress() {
+        System.exit(0);
     }
 
     @FXML private void onHomeTransactionHistoryButtonPress() { }
